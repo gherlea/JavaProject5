9bb833cfa80be0c2e4591f7846811beba1bc64ef
gherlea <101740436+gherlea@users.noreply.github.com>
2022-03-24 05:51:16 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Crina!");
+
+        System.out.println("Salut!");
+
+int x = 2;
+int y = 3;
+        System.out.println(x + y);
+
+        int a = 5;
+        int b = 6;
+        System.out.println(a + b);
+
+
+    }
+}
